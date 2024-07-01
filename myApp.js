fe7985004cf24ba9318647de30ca4fcc6a7ea720
Piyush <vaidpiyush.30@gmail.com>
2024-07-01 21:33:40 +0530

    Start working express server

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
 let express = require("express");
 let app = express();
 console.log("Hello World");
+// get is METHOD
+// "/" is PATH
+// function(req, res) is HANDLER
+// Had to restart for changes to take effect
+app.get("/", function (req, res) {
+  // Serve a string
+  res.send("Hello Express");
+});
 
 module.exports = app;
